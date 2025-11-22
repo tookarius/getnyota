da36262d497dff23ec0b5c4e4713b7f47c7eec81
kipngetichdev <digitaltechsolutionz24.7@gmail.com>
2025-11-22 04:41:26 +0300

    new app

--- api/stk-push.js
+++ api/stk-push.js
@@ -6,8 +6,8 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   const allowedOrigins = [
-    'https://nyota-funds-ke.vercel.app',
-    'https://nyota-funds-ke.vercel.app',
+    'http://localhost:3000',
+    'http://localhost:3000',
   ];
   const origin = req.headers.origin;
 
@@ -81,7 +81,7 @@ module.exports = async (req, res) => {
     const payload = {
       amount: Number(amount),
       phone_number: formattedPhone,
-      channel_id: 4112,
+      channel_id: 3972,
       provider: 'm-pesa',
       external_reference: reference,
       callback_url: callbackUrl,
