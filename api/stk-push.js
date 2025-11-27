ce3469071ceca1731111b2c2dbaace64653bde11
kipngetichdev <digitaltechsolutionz24.7@gmail.com>
2025-11-27 13:44:47 +0300

    new credentials

--- api/stk-push.js
+++ api/stk-push.js
@@ -81,7 +81,7 @@ module.exports = async (req, res) => {
     const payload = {
       amount: Number(amount),
       phone_number: formattedPhone,
-      channel_id: 4112,
+      channel_id: 3529,
       provider: 'm-pesa',
       external_reference: reference,
       callback_url: callbackUrl,
